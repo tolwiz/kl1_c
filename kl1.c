0f3d9653abac2bd89a9b64fb1043ba5c932d83bc
tolwiz <metacirculardispatches@gmail.com>
2025-07-18 12:35:29 +0200

    Added safemalloc and working defr

--- kl1.c
+++ kl1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* Define data structure for atoms. */
-typedef char  Atom;
+typedef char Atom;
 
 /* Define data structure for rules. */
 typedef enum {
@@ -24,6 +24,16 @@ typedef struct {
     int n_atoms_in_body;
     Atom head;
 } DefiniteClause;
+
+/* Safe malloc */
+void *safemalloc(size_t size) {
+    void *ptr = malloc(size);
+    if (!ptr) {
+        perror("malloc failed!");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
 
 /* Print all atoms already performed, A in paper. */
 void printfacts(Atom facts[], int n_atoms_in_facts) {
@@ -51,27 +61,35 @@ void printrule(Rule r) {
 }
 
 /* Function for encoding definite clauses, defr in paper. */
-DefiniteClause *encodedefiniteclauses(Rule rule) {
-    DefiniteClause *defr = malloc(rule.n_atoms_in_head*sizeof(DefiniteClause));
+DefiniteClause *encodedefiniteclauses(Rule rule, int *n_definite_clauses) {
+    int n_defr = rule.n_atoms_in_head;
+
+    if (rule.ruletype == PERMISSIVE) {
+        n_defr += 1; // Add space for the case in which we don't chooose ay
+    }
+
+    DefiniteClause *defr = safemalloc(n_defr*sizeof(DefiniteClause));
 
     char *B = rule.body;
-    
+   
+   int offset = 0;
     if (rule.ruletype == PERMISSIVE) {
-        defr[0].body[0] = '/';
-        defr[0].head = '/';
-        defr[0].n_atoms_in_body = 0; 
+        defr[0].body = safemalloc(sizeof(Atom));
+        defr[0].body[0] = '0';
+        defr[0].head = '0';
+        defr[0].n_atoms_in_body = 1;
+        offset = 1;
     } 
     
     for (int i=0; i<rule.n_atoms_in_head; i++) {
         if (rule.ruletype == PERMISSIVE) {
-            defr[i+1].body = B;
-            defr[i+1].head = rule.head[i]; 
-        } else {
-            defr[i].body = B;
-            defr[i].head = rule.head[i];
-        }
+            defr[i+offset].body = B;
+            defr[i+offset].head = rule.head[i];
+            defr[i+offset].n_atoms_in_body = rule.n_atoms_in_body;
+        }     
     }
 
+    *n_definite_clauses = n_defr;
     return defr;
 }
 
@@ -82,32 +100,32 @@ int main() {
         
     // Add rules to array of rules
     int n_of_rules = 2;
-    Rule *rules = malloc(n_of_rules * sizeof(Rule));
+    Rule *rules = safemalloc(n_of_rules * sizeof(Rule));
 
     // r1
     rules[0].n_atoms_in_body = 2;
-    rules[0].body = malloc(rules[0].n_atoms_in_body*sizeof(char));
+    rules[0].body = safemalloc(rules[0].n_atoms_in_body*sizeof(char));
     rules[0].body[0] = 'p';
     rules[0].body[1] = 'q';
     rules[0].n_atoms_in_head = 2;
-    rules[0].head = malloc(rules[0].n_atoms_in_head*sizeof(char));
+    rules[0].head = safemalloc(rules[0].n_atoms_in_head*sizeof(char));
     rules[0].head[0] = 'r';
     rules[0].head[1] = 's';
     rules[0].ruletype = IMPERATIVE;
 
     // r2
     rules[1].n_atoms_in_body = 2;
-    rules[1].body = malloc(rules[1].n_atoms_in_body*sizeof(char));
+    rules[1].body = safemalloc(rules[1].n_atoms_in_body*sizeof(char));
     rules[1].body[0] = 'r';
     rules[1].body[1] = 's';
     rules[1].n_atoms_in_head = 2;
-    rules[1].head = malloc(rules[1].n_atoms_in_head*sizeof(char));
+    rules[1].head = safemalloc(rules[1].n_atoms_in_head*sizeof(char));
     rules[1].head[0] = 'r';
     rules[1].head[1] = 's';
     rules[1].ruletype = PERMISSIVE;
 
     // Initialize definite clauses
-    //DefiniteClause defr* = malloc(n_of_rules * sizeof(DefiniteClause));
+    //DefiniteClause defr* = safemalloc(n_of_rules * sizeof(DefiniteClause));
 
     printfacts(facts, n_atoms_in_facts);
 
