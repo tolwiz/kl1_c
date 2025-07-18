7183d8a8bc6ea7e766b65f6057c544d24fbe96c2
tolwiz <metacirculardispatches@gmail.com>
2025-07-18 17:47:32 +0200

    Added case for contradiction

--- kl1.c
+++ kl1.c
@@ -84,6 +84,16 @@ DefiniteClause *encodedefiniteclauses(Rule rule, int *n_definite_clauses) {
     DefiniteClause *defr = safemalloc(n_defr*sizeof(DefiniteClause));
     int offset = 0;
     
+    // If head is empty, return an empty clause
+    if (rule.head[0] == '/') {
+        defr[0].body = safemalloc(sizeof(Atom));
+        defr[0].body[0] = '/';
+        defr[0].head = '/';
+        defr[0].n_atoms_in_body = 1;
+        *n_definite_clauses = n_defr;
+        return defr;
+    }
+
     if (rule.ruletype == PERMISSIVE) {
         defr[0].body = safemalloc(sizeof(Atom));
         defr[0].body[0] = '0';
@@ -93,7 +103,6 @@ DefiniteClause *encodedefiniteclauses(Rule rule, int *n_definite_clauses) {
     } 
     
     for (int i=0; i<rule.n_atoms_in_head; i++) {
-        
         if (rule.ruletype == PERMISSIVE) {
             defr[i+offset].n_atoms_in_body = rule.n_atoms_in_body;
             defr[i+offset].body = safemalloc(rule.n_atoms_in_body*sizeof(Atom));
@@ -126,21 +135,28 @@ int main() {
     char facts[] = {'p', 'q', 'r'};
         
     // Add rules to array of rules
-    int n_of_rules = 2;
+    int n_of_rules = 3;
     Rule *rules = safemalloc(n_of_rules * sizeof(Rule));
 
     Atom r1_body[] = { 'p', 'q' };
     Atom r1_head[] = { 'r', 's' };
     rules[0] = encoderule(2, 2, r1_body, r1_head, IMPERATIVE);
+
     Atom r2_body[] = { 'r', 's' }; 
     Atom r2_head[] = { 'r', 's' };
     rules[1] = encoderule(2, 2, r2_body, r2_head, PERMISSIVE);
     
+    Atom r3_body[] = { 'x', 'y' }; 
+    Atom r3_head[] = { '/' };
+    rules[2] = encoderule(2, 1, r3_body, r3_head, IMPERATIVE);
+    
     // Initialize definite clauses
     int n_clauses1;
     int n_clauses2;
+    int n_clauses3;
     DefiniteClause *defr1 = encodedefiniteclauses(rules[0], &n_clauses1);
     DefiniteClause *defr2 = encodedefiniteclauses(rules[1], &n_clauses2);
+    DefiniteClause *defr3 = encodedefiniteclauses(rules[2], &n_clauses3);
     
     printfacts(facts, n_atoms_in_facts);
 
@@ -148,6 +164,7 @@ int main() {
 
     printdefiniteclauses(defr1, n_clauses1);
     printdefiniteclauses(defr2, n_clauses2);
+    printdefiniteclauses(defr3, n_clauses3);
 
     for (int i=0; i<n_of_rules; i++) {
         free(rules[i].body);
@@ -157,3 +174,4 @@ int main() {
 
     return 0;
 }
+
